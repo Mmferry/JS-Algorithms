c3a6eef8d197e586d7e9980bf62caccabcb636ab
Muhammad Farid <mfaired@gmail.com>
2020-07-12 12:46:06 +0000

    Harmless Ransom note algorithms

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-function FizzBuzz(num) {
-  for(var i = 1; i <= num; i++){
-    if( i % 15 === 0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log(i);
-  }
-}
+// Linrer Time Complexity O(n)
+function harmlessRansomNote (noteText, magazineText) {
+  let noteArr = noteText.split(' ');
+  let magazineArr = magazineText.split(' ');
+  let magazineObj = {};
+  //O(n)
+  magazineArr.forEach(word => {
+    if(!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word] ++;
+  });
 
-FizzBuzz(30);
+  let noteIsPossible = true;
+  //O(m)
+  noteArr.forEach(word => {
+    if(magazineObj[word]){
+      magazineObj[word]--;
+      if(magazineObj[word] < 0) noteIsPossible = false;
+    } else noteIsPossible = false;
+  });
+
+  return noteIsPossible;
+}
+//O(n + m)
+harmlessRansomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited');
