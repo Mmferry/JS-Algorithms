b3ee9d3addb814a17731d9813265878d9e51d300
Muhammad Farid <mfaired@gmail.com>
2020-07-16 12:49:32 +0000

    meanMedianMode

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-function reverseArrayInPlace(arr) {
-  for (var i = 0; i < arr.length / 2; i++) {
-    var tempVar = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = tempVar;
+function meanMedianMode(array) {
+  return {
+    mean: getMean(array),
+    median: getMedian(array),
+    mode: getMode(array)
   }
-  
-  return arr;
 }
  
-reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]);
+function getMean(array) {
+  var sum = 0;
+  
+  array.forEach(num => {
+    sum += num;
+  });
+  
+  var mean = sum / array.length;
+  return mean;
+}
+ 
+function getMedian(array) {
+  array.sort(function(a, b){return a-b});
+  var median;
+  
+  if (array.length % 2 !== 0) {
+    median = array[Math.floor(array.length / 2)];
+  }
+  else {
+    var mid1 = array[(array.length / 2) - 1];
+    var mid2 = array[array.length / 2];
+    median = (mid1 + mid2) / 2;
+  }
+  
+  return median;
+}
+ 
+function getMode(array) {
+  var modeObj = {};
+  
+  // create modeObj
+  array.forEach(num => {
+    if (!modeObj[num]) modeObj[num] = 0;
+    modeObj[num]++;
+  });
+  
+  // create array of mode/s 
+  var maxFrequency = 0;
+  var modes = [];
+  for (var num in modeObj) {
+    if (modeObj[num] > maxFrequency) {
+      modes = [num];
+      maxFrequency = modeObj[num];
+    }
+    else if (modeObj[num] === maxFrequency) modes.push(num);
+  }
+  // if every value appears same amount of times 
+  if (modes.length === Object.keys(modeObj).length) modes = [];
+  return modes;
+}
+ 
+ 
+meanMedianMode([9,10,23,10,23,9]);
