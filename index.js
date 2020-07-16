92e2f9779ca9c081f6a53c44cb738048c78c45b0
Muhammad Farid <mfaired@gmail.com>
2020-07-16 10:13:51 +0000

    reverse word

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-function caesarCipher(str, num) {
-  num = num % 26;
-  let lowerCaseString = str.toLowerCase();
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  let newString = '';
+function reverseWords(string) {
+  let wordArr = string.split(' ');
+  let reversedWordsArr = [];
 
-  for(let i = 0; i < lowerCaseString.length; i++) {
-    let currentLatter = lowerCaseString[i];
-    if(currentLatter === '') {
-      newString += currentLatter;
-      continue;
+  wordArr.forEach(word => {
+    let reversedWord = '';
+    for(let i = word.length - 1; i >= 0; i--){
+      reversedWord += word[i];
     }
 
-    let currentIndex = alphabet.indexOf(currentLatter);
-    let newIndex = currentIndex + num;
-    if(newIndex > 25) newIndex = newIndex - 26;
-    if(newIndex < 0) newIndex = 26 + newIndex;
-    if(str[i] === str[i].toUpperCase()) {
-      newString += alphabet[newIndex].toUpperCase();
-    } else newString += alphabet[newIndex];
-  };
+    reversedWordsArr.push(reversedWord);
+  });
 
-  return newString;
+  return reversedWordsArr.join(' ');
 }
 
-caesarCipher('Zoo Keeper', 2);
+reverseWords('Mohammed Fared');
