fe2d90b4369867d36afaddbefbdb7e564175bdfa
Muhammad Farid <mfaired@gmail.com>
2020-07-18 16:21:06 +0000

    Memoized Fibonacci Algorithms

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function fibonacci(postion) {
-  if (postion < 3) return 1;
-  else return fibonacci(postion - 1) + fibonacci(postion - 2)
+function fibMemo(index, cache) {
+  cache = cache || [];
+  if (cache[index]) return cache[index];
+  else {
+    if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    }
+  }
+  return cache[index];
 }
-
-fibonacci(20);
+ 
+fibMemo(50);
