8446631d046842d5ed0a61ad6bdf9fda4afa5fd1
Muhammad Farid <mfaired@gmail.com>
2020-07-19 08:59:58 +0000

    Max Stock Profit algorithms

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function mergeSort(arr) {
-  if(arr.length < 2) return arr;
-  var middleIndex = arr.length / 2;
-  var firstHalf = arr.slice(0, middleIndex);
-  var secondHalf = arr.slice(middleIndex);
-
-  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
-}
-
-function merge(arr1, arr2) {
-  var result = [];
-  while(arr1.length && arr2.length){
-    var minElm;
-    if(arr1[0] < arr2[0]) minElm = arr1.shift();
-    else minElm = arr2.shift();
-    result.push(minElm);
+function maxStockProfit (pricesArr) {
+  var maxProfit = -1;
+  var buyPrice = 0;
+  var sellPrice = 0;
+  
+  var changeBuyPrice = true;
+  
+  for (var i = 0; i < pricesArr.length; i++) {
+    if (changeBuyPrice) buyPrice = pricesArr[i];
+    sellPrice = pricesArr[i + 1];
+    
+    if (sellPrice < buyPrice) {
+      changeBuyPrice = true;
+    }
+    else {
+      var tempProfit = sellPrice - buyPrice;
+      if (tempProfit > maxProfit) maxProfit = tempProfit;
+      changeBuyPrice = false;
+    }
   }
-
-  if(arr1.length) result = result.concat(arr1);
-  else result = result.concat(arr2);
-
-  return result;
+  
+  return maxProfit;
 }
-
-mergeSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 1, 9, 1]);
+ 
+maxStockProfit([10, 18, 4, 5, 9, 6, 16, 12]);
